0d20e178c66a716c1fe96add0f9246b09ae0a806
Florian Boulnois <fboulnois@mgh.harvard.edu>
2015-09-04 10:13:32 -0400

    feat: Generate basic microarray report

---
+++ R/RenderReport.R
@@ -0,0 +1,8 @@
+render.report <- function(config, eset, tf)
+{
+  config <- read.yaml.config(config)
+
+  infile  <- normalizePath(system.file("extdata", "report.rmd", package = "made"), mustWork = TRUE)
+  outfile <- normalizePath(file.path(dirname(config$groups$group_file), "microarray_report.html"), mustWork = FALSE)
+  rmarkdown::render(input = infile, output_file = outfile)
+}
