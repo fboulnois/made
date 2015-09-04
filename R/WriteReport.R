48092cffb2819882ca18aa58b389ef3d20db7295
Florian Boulnois <fboulnois@mgh.harvard.edu>
2015-09-04 11:27:54 -0400

    fix: Ensure that eset and tf are passed to function

--- R/WriteReport.R
+++ R/WriteReport.R
@@ -2,6 +2,16 @@ write.report <- function(config, eset, tf)
 {
   config <- read.yaml.config(config)
 
+  if(missing(eset))
+  {
+    stop("Expression set is required to write report.")
+  }
+
+  if(missing(tf))
+  {
+    stop("List of top tables is required to write report.")
+  }
+
   infile  <- normalizePath(system.file("extdata", "report.rmd", package = "made"), mustWork = TRUE)
   outfile <- normalizePath(file.path(dirname(config$groups$group_file), "microarray_report.html"), mustWork = FALSE)
   rmarkdown::render(input = infile, output_file = outfile)
