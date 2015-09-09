2ee24d7ffd2d7dbc93aafbbf4a7c430c0f5a3989
Florian Boulnois <fboulnois@mgh.harvard.edu>
2015-09-09 11:18:20 -0400

    docs: Document write.report

--- R/WriteReport.R
+++ R/WriteReport.R
@@ -1,3 +1,20 @@
+#' Write microarray report
+#'
+#' Generate the final microarray report.
+#'
+#' Generates the microarray report summarizing the information in the
+#' experiment. The information in the report depends on the config and the
+#' packages available.
+#'
+#' @param config Character string consisting of the path to the configuration
+#' file generated using the \code{write.yaml.config} function or parsed
+#' configuration list associated with a microarray experiment.
+#'
+#' @param eset Expression set object describing microarray experiment at the
+#' level of probes.
+#'
+#' @param tf List of top tables for each comparison of interest and their genes,
+#' associated log-fold changes, and statistical values.
 write.report <- function(config, eset, tf)
 {
   config <- read.yaml.config(config)
@@ -9,7 +26,7 @@ write.report <- function(config, eset, tf)
 
   if(missing(tf))
   {
-    stop("List of top tables is required to write report.")
+    stop("List of gene top tables is required to write report.")
   }
 
   infile  <- normalizePath(system.file("extdata", "report.rmd", package = "made"), mustWork = TRUE)
